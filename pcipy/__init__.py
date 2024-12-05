2f8f2a000ce4a075f5f49d14814c2ee330bb1d90
Eleonora Castelli <ell.castelli@gmail.com>
2024-12-05 15:22:12 -0500

    Add initial modules

---
+++ pcipy/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 12:30:49 2024
+
+@author: E Castelli
+"""
---
+++ pcipy/apci_core.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 12:31:37 2024
+
+@author: Q Baghi, J Baker, E Castelli
+"""
+
