de2c851a3fb4550fba07017b50a0b1518a7e69d8
John Baker <john.g.baker@nasa.gov>
2025-01-16 18:58:13 +0000

    AAS poster version. This commit includes the current snapshot of the codes and notebooks from which I took material for the AAS245 poster.

--- pcipy/channel_analysis.py
+++ pcipy/channel_analysis.py
@@ -11,7 +11,7 @@ from . import plotting
 import matplotlib.pyplot as plt
 import numpy as np
 
-def stationarity_plots(filter_obj, ydata, title='',ndens=1000,split_orders=False,nchan=None,zero_mean=False,detrend=False):
+def pci_stationarity_plots(filter_obj, ydata, title='',ndens=1000,split_orders=False,nchan=None,zero_mean=False,detrend=False):
     '''
     Make time-domain plots to explore PCI channel stationarity
 
@@ -65,7 +65,7 @@ def stationarity_plots(filter_obj, ydata, title='',ndens=1000,split_orders=False
                             ticklabelsize=20, style='publication',
                             fontfamily = 'STIXGeneral')
         nf=len(filters)
-        fig1, axs = plt.subplots(nrows=order+2,ncols=len(filters),squeeze=False,figsize=[6.4*nf, 4.8*((order+2))],sharex=True,sharey=True)
+        fig1, axs = plt.subplots(nrows=order+2,ncols=len(filters),squeeze=False,figsize=[6.4*nf, 4.8*((order+2))],sharex="col",sharey="row")
         for ax in axs.flat:
             ax.label_outer()
             
@@ -86,40 +86,94 @@ def stationarity_plots(filter_obj, ydata, title='',ndens=1000,split_orders=False
             
         plt.show()
 
+def stationarity_plots(channel_sets, title='',ndens=1000,split_orders=False,nchan=None):
+    '''
+    Make time-domain plots to explore PCI channel stationarity
+
+    Arguments:
+       channel_sets list(ndarray)  Each array of lenght of the number of channels
+    Optional:
+       title        str       Plot title
+       ndens        int       target number of samples to show in each plot curve
+                              or 0 for all samples
+       
+    '''
+    sets=channel_sets
+    if not isinstance(sets,list):
+        sets=[sets]
+    if not isinstance(title,list):
+        title=[title]*len(sets)
+
+    if nchan is None:
+        nchan=len(sets[0])
+    
+    plotting.plotconfig(lbsize=20, lgsize=16, fsize=18, 
+                    ticklabelsize=20, style='publication',
+                    fontfamily = 'STIXGeneral')
+    fig1, axs = plt.subplots(ncols=len(sets),squeeze=False,sharey=True)
+    axs=axs[0]
+    for i,x in enumerate(sets):
+        print("i=",i)
+        for ich in range(nchan):
+            #xch=xf.channels[-(ich+1)]
+            xch=x[-(ich+1)]
+            
+            if ndens==0:
+                ev=1
+            else:
+                ev=len(xch)//ndens+1
+            axs[i].plot(xch[::ev],label='c'+str(ich),zorder=nchan-ich)
+            axs[i].set_title(title[i])
+            #check var:
+            chmean=np.mean(xch)
+            print('mean:',chmean)
+            print('variance: ',sum((xch-chmean)**2)/(len(xch)-1))
+            axs[i].legend()
+    plt.show()
 
 
-def temporal_variance_corr_plots(filter_obj, ydata, title='',ndens=1000,split_orders=False,nchan=None,zero_mean=False,detrend=False):
+def pci_temporal_variance_corr_plots(filter_obj, ydata, title='',ndens=1000,nchan=None,zero_mean=False,detrend=False):
     filters=filter_obj
     if not isinstance(filters,list):
         filters=[filters]
-    if not isinstance(title,list):
-        title=[title]*len(filters)
-    nplot=nchan
-    nf=len(filters)
-    
     if nchan is None:
         nchan=filters[0].maxcompts
+
+    sets=[np.array(xf.apply_for_channels(ydata, n_channels=nchan,zero_mean=zero_mean,detrend=detrend)) for xf in filters]
+    temporal_variance_corr_plots(sets, title='',ndens=ndens,nchan=nchan)
+
+def temporal_variance_corr_plots(channel_sets, title='',ndens=1000,nchan=None):
+    sets=channel_sets
+    if not isinstance(sets,list):
+        sets=[sets]
+    if not isinstance(title,list):
+        title=[title]*len(sets)
+    nplot=nchan
+    nf=len(sets)
+    
+    if nchan is None:
+        nchan=len(sets[0])
 
     plotting.plotconfig(lbsize=20, lgsize=16, fsize=18, 
                     ticklabelsize=20, style='publication',
                     fontfamily = 'STIXGeneral')
-    fig1, axs1 = plt.subplots(ncols=len(filters),nrows=1,squeeze=False,sharey=True,figsize=[6.4*nf, 4.8*((1))])
-    fig1, axs2 = plt.subplots(ncols=len(filters),nrows=nplot,squeeze=False,sharey=True,figsize=[6.4*nf, 4.8*((nplot))])
+    fig1, axs1 = plt.subplots(ncols=len(sets),nrows=1,squeeze=False,sharey=True,figsize=[6.4*nf, 4.8*((1))])
+    fig1, axs2 = plt.subplots(ncols=len(sets),nrows=nplot-1,squeeze=False,sharey=True,figsize=[6.4*nf, 4.8*((nplot))])
 
-    for i,xf in enumerate(filters):
+    for i,x in enumerate(sets):
         print("i=",i)
-        x=np.array(xf.apply_for_channels(ydata, n_channels=nchan,zero_mean=zero_mean,detrend=detrend))
+
         #we compute channel covariance in batches of size nbatch
         print('plotting',nplot,'of',nchan,'channels')
         #trim the data to a multiple of nbatch
         nbatch=1000
         ndata=len(x.T)
         ntrim=(ndata//nbatch)*nbatch
-        binned=x[:,:ntrim].reshape(nchan,-1,nbatch)
+        binned=x[:nchan,:ntrim].reshape(nchan,-1,nbatch)
         if True:
             print("subtracting bin means!")
             binned=(binned.T-binned.mean(axis=2).T).T
-        #print(binned.shape)
+        print('shape of input,trimmed,binned',x.shape,x[:,:ntrim].shape,binned.shape)
         print(ntrim//nbatch,'batches of size',nbatch)
         #print(binned[0,0:2])
         if ndens==0:
@@ -132,10 +186,10 @@ def temporal_variance_corr_plots(filter_obj, ydata, title='',ndens=1000,split_or
         ax=axs1[0,i]
         stdevs=np.sqrt(np.mean(binned**2,axis=2))
         for j in range(nplot):
-            jj=nchan-1-j
-            print('stdevs shape',stdevs.shape,'ev',ev)
-            print(stdevs[jj,::ev].shape)
-            ax.semilogy(stdevs[jj,::ev], marker='.',linewidth=1, label=r'$\mathrm{ch}$[' + str(j+1) + ']')
+            #jj=nchan-1-j
+            #print('stdevs shape',stdevs.shape,'ev',ev)
+            #print(stdevs[j,::ev].shape)
+            ax.semilogy(stdevs[j,::ev], marker='.',linewidth=1, label=r'$\mathrm{ch}$[' + str(j+1) + ']')
         ax.legend(loc='upper left', ncol=2)
         ax.grid(linewidth=1, which='both', color='gray', linestyle='dotted')
         ax.set_xlabel("sample")
@@ -143,16 +197,16 @@ def temporal_variance_corr_plots(filter_obj, ydata, title='',ndens=1000,split_or
         ax.set_title(title)
         #plt.show()
         for k in range(nplot-1):                    
-            kk=nchan-1-k                
+            #kk=nchan-1-k                
             #title='Channel cross correlation vs channel '+str(k+1)+' for '+plots_label
             ax=axs2[k,i]
-            for j in range(nplot-k):
-                jj=kk-j
-                covar=np.mean(binned[jj]*binned[kk],axis=1)
+            for j in range(k,nplot):
+                #jj=kk-j
+                covar=np.mean(binned[j]*binned[k],axis=1)
                 #print(jj,kk,covar[1],stdevs[jj,1],stdevs[kk,1],stdevs[jj,1]*stdevs[kk,1])
                 #print((binned[jj]*binned[kk])[:,1])
                 #print(covar.shape)
-                ax.plot((covar/stdevs[jj]/stdevs[kk])[::ev], marker='.',linewidth=1, label=r'$\mathrm{ch}$[' + str(nchan-kk)+','+str(nchan-jj)+ ']')
+                ax.plot((covar/stdevs[j]/stdevs[k])[::ev], marker='.',linewidth=1, label=r'$\mathrm{ch}$[' + str(k+1)+','+str(j+1)+ ']')
             ax.legend(loc='upper left', ncol=2)
             ax.grid(linewidth=1, which='both', color='gray', linestyle='dotted')
             ax.set_xlabel("sample")
