fd69d6f1f47142d216837a0cfabfcf5d749ae6db
Eleonora Castelli <ell.castelli@gmail.com>
2025-03-18 14:41:37 -0400

    update requirements

--- setup.py
+++ setup.py
@@ -38,8 +38,16 @@ setup(
         keywords='principal component analysis, gravitational-wave interferometry, \
             LISA',
         # python_requires='>=3.5',
-        install_requires=['numpy', 'xarray', 'scipy', 'sympy', 
-        'pyfftw', 'h5py', 'scikit-learn'],
+        install_requires=['numpy', 
+                          'xarray', 
+                          'scipy', 
+                          'sympy', 
+                          'pyfftw', 
+                          'h5py', 
+                          'scikit-learn', 
+                          'lisainstrument>=1.8',
+                          'lisagwresponse',
+                          'pytdi'],
         # needs to be installed along with your package. Eg: 'caer'
 
         classifiers= [
