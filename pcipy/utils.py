d14b9494a039df481c570739bd2706c7b605e802
Eleonora Castelli <ell.castelli@gmail.com>
2025-05-13 16:07:10 -0400

    Add LogLPFPsd implementation of CPSD by Lorenzo Sala

--- pcipy/utils.py
+++ pcipy/utils.py
@@ -7,12 +7,15 @@ Created on Wed Dec 11 14:18:38 2024
 """
 
 import numpy as np
+import scipy.special as sp
 from scipy import interpolate
 
 def estimatePSD(data, dt, Nmax, fmax, axs = None):
     '''
     Estimate PSD of a given data stretch, by interpolating the log-periodogram of the data DFT.
     Uses minimally correlated frequencies according to the CPSD estimation algorithm originally published in PhysRevLett.120.061101
+    
+    L Sala, Oct21/Sept22
 
     Parameters
     ----------
@@ -100,9 +103,241 @@ def estimatePSD(data, dt, Nmax, fmax, axs = None):
 
     return noisecoeffs
 
+
+class PSDstats():
+    """
+    PSDstats, compute the one-sided Cross Power Spectral Density (CPSD) matrix
+        at minimally correlated frequencies, averaging over periodograms.
+        Compute cross-coherences.
+    L Sala, Oct21/Sept22
+
+    Parameters
+    ----------
+    datamat : TYPE
+        tuple of synchronously sampled time series
+    Tmax : TYPE
+        max time length of periodograms
+    fmax : TYPE
+        max frequency to be computed
+    fs : TYPE
+        sampling frequency
+     win:       
+        spectral window (function) or None. If None, BH92 is used
+
+    Returns
+    -------
+    None.
+
+    """
+    def __init__(self,datatuple,Tmax,fmax,fs,win=None,units='',olap=50,detrend=False,getperiodograms=False,c=0.68):
+        datamat = np.asarray(datatuple)
+        if(datamat.ndim==1):
+            datamat = np.reshape(datamat,(1,datamat.size))
+        if detrend: datamat = datamat - np.mean(datamat,axis=1,keepdims=True)
+        win = BH92 if win is None else win
+        initcheck(datamat,Tmax,fmax,fs)
+        self.fs = fs
+        self.olap = olap
+        self.units = units
+        self.L, self.freqs = choosefreqs(Nmax=Tmax*fs,fmax=fmax,fs=fs)
+        if getperiodograms:
+            self.periodograms, self.navs = getPeriodograms(datamat=datamat,L=self.L,freqs=self.freqs,fs=fs,win=win,olap=olap)
+        self.CPSD, self.navs = getCPSD(datamat=datamat,L=self.L,freqs=self.freqs,fs=fs,win=win,olap=olap)
+        self.cohere = getcohere(CPSD=self.CPSD)
+
+    def getPSDerrors(self,idx=0,cval=0.68):
+        '''
+        L Sala, Oct21/Sept22
+
+        Parameters
+        ----------
+        datamat : TYPE
+            DESCRIPTION.
+        Tmax : TYPE
+            DESCRIPTION.
+        fmax : TYPE
+            DESCRIPTION.
+        fs : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        '''
+        Sxx  = np.real(self.CPSD[:,idx,idx])
+        Smin = self.navs*Sxx/sp.gammainccinv(self.navs,(1-cval)/2)
+        Smax = self.navs*Sxx/sp.gammainccinv(self.navs,(1+cval)/2)
+        return Sxx,Smin,Smax
+
+def BH_lowpass(data, t_win=100,t_sam=5,fs=10):
+    '''
+    Lowpass data by convolving with a BH92 windowing function.
+    L Sala, December 2021
+    modified by E Castelli, Sept 2022
+
+    Parameters
+    ----------
+    data : rec-array
+        Synchronously sampled time series with fields t', 'A', 'E', 'T'.
+    t_win : TYPE, optional
+        Window length, controls cut frequency. The default is 100.
+    t_sam : TYPE, optional
+        Output sampling time. The default is 5.
+    fs : TYPE, optional
+        Input sampling frequency. The default is 10.
+
+    Returns
+    -------
+    datalp : rec-array
+        Low-passed time series with fields 't', 'A', 'E', 'T'.
+    '''
+    names = data.dtype.names[1:]
+    dt = 1/fs
+    step_win = np.intc(t_win*fs)
+    step_sam = np.intc(t_sam*fs)
+    assert t_sam>=dt, 'Watch out, do not upsample your data.'
+    assert np.isclose(t_sam*fs,int(t_sam*fs),rtol=1e-5), 'Downsampling time must be multiple of sampling time.'
+    assert np.isclose(t_win*fs,int(t_win*fs),rtol=1e-5), 'Windowing time must be multiple of sampling time.'
+    assert np.isclose(step_win/step_sam,int(step_win/step_sam),rtol=1e-5), 'Watch out, t_win must be multiple of t_sam.'
+    
+    dtarr = np.diff(data['t'])
+    assert np.isclose(dtarr[0],dt,rtol=1e-5), 'Aaargh, sampling frequency is not consistent with data.' #just check fs
+    assert np.allclose(dtarr,dt,rtol=1e-5), 'Aaargh, your data are not equally sampled in time.' #just check sampling time
+
+    BHfilt = BH92(step_win) #build filter
+    BHarea = np.sum(BHfilt)
+    BHfilt = BHfilt/BHarea
+    onearray = np.ones(step_win)/step_win
+
+    #apply filter convolving
+    outts = [np.convolve(data['t'],onearray,mode='valid')] #just a simple way to get times, computationally more expensive than linspace, but safer
+    for tdi in names:
+        outts += [np.convolve(data[tdi], BHfilt,  mode='valid')]
+    #downsample it
+    for i in range(len(outts)):
+        outts[i] = outts[i][::step_sam]
+    datalp = np.rec.fromarrays(outts, names = ['t', 'A', 'E', 'T'])
+    return datalp
+
+def BH92(M:int):
+    '''
+    Mathematical expression of a Blackman-Harris window.
+    
+    Parameters
+    ----------
+    M : int
+        Length of BH window.
+
+    Returns
+    -------
+    ndarray
+        Array of values for the BH window of desired length.
+
+    '''
+    z = np.arange(0,M)*2*np.pi/M
+    return 0.35875 - 0.48829 * np.cos(z) + 0.14128 * np.cos(2*z) - 0.01168 * np.cos(3*z)
+
+# def bh_lowpass(data,t_win=100,t_sam=5,fs=10):
+#     '''
+#     Lowpass data by convolving with a BH92 windowing function.
+#     L Sala, December 2021
+#     modified by E Castelli, Sept 2022
+
+#     Parameters
+#     ----------
+#     data : rec-array
+#         Synchronously sampled time series with fields t', 'A', 'E', 'T'.
+#     t_win : TYPE, optional
+#         Window length, controls cut frequency. The default is 100.
+#     t_sam : TYPE, optional
+#         Output sampling time. The default is 5.
+#     fs : TYPE, optional
+#         Input sampling frequency. The default is 10.
+
+#     Returns
+#     -------
+#     datalp : rec-array
+#         Low-passed time series with fields 't', 'A', 'E', 'T'.
+#     '''
+
+#     names = data.dtype.names[1:]
+#     dt = 1/fs
+#     step_win = np.intc(t_win*fs)
+#     step_sam = np.intc(t_sam*fs)
+#     assert t_sam>=dt, 'Watch out, do not upsample your data.'
+#     assert np.isclose(t_sam*fs,int(t_sam*fs),rtol=1e-5), 'Downsampling time must be multiple of sampling time.'
+#     assert np.isclose(t_win*fs,int(t_win*fs),rtol=1e-5), 'Windowing time must be multiple of sampling time.'
+#     assert np.isclose(step_win/step_sam,int(step_win/step_sam),rtol=1e-5), 'Watch out, t_win must be multiple of t_sam.'
+    
+#     dtarr = np.diff(data['t'])
+#     assert np.isclose(dtarr[0],dt,rtol=1e-5), 'Aaargh, sampling frequency is not consistent with data.' #just check fs
+#     assert np.allclose(dtarr,dt,rtol=1e-5), 'Aaargh, your data are not equally sampled in time.' #just check sampling time
+
+#     BHfilt = bh92(step_win) #build filter
+#     BHarea = np.sum(BHfilt)
+#     BHfilt = BHfilt/BHarea
+#     onearray = np.ones(step_win)/step_win
+
+#     #apply filter convolving
+#     outts = [np.convolve(data['t'],onearray,mode='valid')] #just a simple way to get times, computationally more expensive than linspace, but safer
+#     for tdi in names:
+#         outts += [np.convolve(data[tdi], BHfilt,  mode='valid')]
+#     #downsample it
+#     for i in range(len(outts)):
+#         outts[i] = outts[i][::step_sam]
+#     datalp = np.rec.fromarrays(outts, names = ['t', 'A', 'E', 'T'])
+#     return datalp
+
+
+# def bh92(M:int):
+#     '''
+#     Mathematical expression of a Blackman-Harris window.
+    
+#     Parameters
+#     ----------
+#     M : int
+#         Length of BH window.
+
+#     Returns
+#     -------
+#     ndarray
+#         Array of values for the BH window of desired length.
+
+#     '''
+#     z = np.arange(0,M)*2*np.pi/M
+#     return 0.35875 - 0.48829 * np.cos(z) + 0.14128 * np.cos(2*z) - 0.01168 * np.cos(3*z)
+
+def initcheck(datamat,Tmax,fmax,fs):
+    '''
+    L Sala, Oct21/Sept22
+
+    Parameters
+    ----------
+    datamat : TYPE
+        DESCRIPTION.
+    Tmax : TYPE
+        DESCRIPTION.
+    fmax : TYPE
+        DESCRIPTION.
+    fs : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
+    npoints = datamat.shape[1]
+    # assert(Tmax*fs<npoints)
+    assert(fmax<=fs/2)
+    return
+
+
 def choosefreqs(Nmax:int,fmax:float,fs:float):
     '''
     Minimally correlated frequencies selection, according to the CPSD estimation algorithm originally published in PhysRevLett.120.061101 and Supplemental Material.
+    
 
     Returns 
 
@@ -125,9 +360,9 @@ def choosefreqs(Nmax:int,fmax:float,fs:float):
 
     M = 4
     r = 3/5
-    f = []
+    f = [M/(Nmax/fs)] # []
     L = [Nmax]
-    k=1
+    k=2 # 1
     while(1):
         tmpL = r**(k-2) * Nmax
         tmpf = 2*M/(tmpL/fs);
@@ -138,73 +373,131 @@ def choosefreqs(Nmax:int,fmax:float,fs:float):
         k += 1
     return np.asarray(L,dtype=int),np.asarray(f)
 
-
-def bh_lowpass(data,t_win=100,t_sam=5,fs=10):
+def getCPSD(datamat,L,freqs,fs,win,olap):
     '''
-    Lowpass data by convolving with a BH92 windowing function.
-    L Sala, December 2021
-    modified by E Castelli, Sept 2022
+    L Sala, Oct21/Sept22
 
     Parameters
     ----------
-    data : rec-array
-        Synchronously sampled time series with fields t', 'A', 'E', 'T'.
-    t_win : TYPE, optional
-        Window length, controls cut frequency. The default is 100.
-    t_sam : TYPE, optional
-        Output sampling time. The default is 5.
-    fs : TYPE, optional
-        Input sampling frequency. The default is 10.
+    datamat : TYPE
+        DESCRIPTION.
+    Tmax : TYPE
+        DESCRIPTION.
+    fmax : TYPE
+        DESCRIPTION.
+    fs : TYPE
+        DESCRIPTION.
 
     Returns
     -------
-    datalp : rec-array
-        Low-passed time series with fields 't', 'A', 'E', 'T'.
+    None.
+
     '''
+    CPSD = []
+    navs = []
+    for (tmpL,tmpf) in zip(L,freqs):
+        tmpCPSD,tmpnavs,_ = getCPSD_1freq(datamat=datamat,tmpL=tmpL,tmpf=tmpf,fs=fs,win=win,olap=olap)
+        CPSD.append(tmpCPSD)
+        navs.append(tmpnavs)
+    return np.asarray(CPSD),np.asarray(navs)
 
-    names = data.dtype.names[1:]
-    dt = 1/fs
-    step_win = np.intc(t_win*fs)
-    step_sam = np.intc(t_sam*fs)
-    assert t_sam>=dt, 'Watch out, do not upsample your data.'
-    assert np.isclose(t_sam*fs,int(t_sam*fs),rtol=1e-5), 'Downsampling time must be multiple of sampling time.'
-    assert np.isclose(t_win*fs,int(t_win*fs),rtol=1e-5), 'Windowing time must be multiple of sampling time.'
-    assert np.isclose(step_win/step_sam,int(step_win/step_sam),rtol=1e-5), 'Watch out, t_win must be multiple of t_sam.'
-    
-    dtarr = np.diff(data['t'])
-    assert np.isclose(dtarr[0],dt,rtol=1e-5), 'Aaargh, sampling frequency is not consistent with data.' #just check fs
-    assert np.allclose(dtarr,dt,rtol=1e-5), 'Aaargh, your data are not equally sampled in time.' #just check sampling time
-
-    BHfilt = bh92(step_win) #build filter
-    BHarea = np.sum(BHfilt)
-    BHfilt = BHfilt/BHarea
-    onearray = np.ones(step_win)/step_win
-
-    #apply filter convolving
-    outts = [np.convolve(data['t'],onearray,mode='valid')] #just a simple way to get times, computationally more expensive than linspace, but safer
-    for tdi in names:
-        outts += [np.convolve(data[tdi], BHfilt,  mode='valid')]
-    #downsample it
-    for i in range(len(outts)):
-        outts[i] = outts[i][::step_sam]
-    datalp = np.rec.fromarrays(outts, names = ['t', 'A', 'E', 'T'])
-    return datalp
-
-
-def bh92(M:int):
+def getPeriodograms(datamat,L,freqs,fs,win,olap):
     '''
-    Mathematical expression of a Blackman-Harris window.
-    
+    L Sala, Oct21/Sept22
+
     Parameters
     ----------
-    M : int
-        Length of BH window.
+    datamat : TYPE
+        DESCRIPTION.
+    Tmax : TYPE
+        DESCRIPTION.
+    fmax : TYPE
+        DESCRIPTION.
+    fs : TYPE
+        DESCRIPTION.
 
     Returns
     -------
-    ndarray
-        Array of values for the BH window of desired length.
+    None.
 
     '''
-    z = np.arange(0,M)*2*np.pi/M
-    return 0.35875 - 0.48829 * np.cos(z) + 0.14128 * np.cos(2*z) - 0.01168 * np.cos(3*z)
+    periodograms = []
+    navs = []
+    for (tmpL,tmpf) in zip(L,freqs):
+        _,tmpnavs,tmpPeriodograms = getCPSD_1freq(datamat=datamat,tmpL=tmpL,tmpf=tmpf,fs=fs,win=win,olap=olap)
+        periodograms.append(tmpPeriodograms)
+        navs.append(tmpnavs)
+    return periodograms,np.asarray(navs)
+
+def getCPSD_1freq(datamat,tmpL,tmpf,fs,win,olap):
+    '''
+    L Sala, Oct21/Sept22
+
+    Parameters
+    ----------
+    datamat : TYPE
+        DESCRIPTION.
+    Tmax : TYPE
+        DESCRIPTION.
+    fmax : TYPE
+        DESCRIPTION.
+    fs : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
+    ndim = datamat.shape[0]
+    npoints = datamat.shape[1]
+
+    # DFT coefficients
+    winpt = win(tmpL) #spectral window
+    p = -2j*np.pi*np.arange(0,tmpL,1)/fs;
+    C = np.exp(tmpf*p); #DFT coefficients
+
+    startpoint = 0
+    tmpnavs = 0
+    Amat = np.zeros((ndim,ndim),dtype=complex)
+    periodograms = []
+    while(1):
+        endpoint = startpoint + tmpL
+        if(endpoint > npoints):
+            break
+        xs0 = datamat[:,startpoint:endpoint] #multivariate stretch
+        xs = winpt * xs0 #windowed multivariate stretch
+        ax = xs @ C #complex periodogram
+        periodograms.append(ax)
+        Amat += np.outer(ax,ax.conj()) #fill CPSD matrix
+        tmpnavs += 1
+        startpoint += int(tmpL*(100-olap)/100)
+
+    wins2 = winpt@winpt
+    tmpCPSD  = 2.0*Amat/tmpnavs/fs/wins2; #one-sided CPSD matrix
+    periodograms  = np.asarray(periodograms)*np.sqrt(2.0/fs/wins2); #one-sided periodograms
+    return tmpCPSD,tmpnavs,periodograms
+
+def getcohere(CPSD):
+    '''
+    L Sala, Oct21/Sept22
+
+    Parameters
+    ----------
+    datamat : TYPE
+        DESCRIPTION.
+    Tmax : TYPE
+        DESCRIPTION.
+    fmax : TYPE
+        DESCRIPTION.
+    fs : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
+    cohere = [ np.diag(np.diag(Sij)**(-1/2)) @ Sij @ np.diag(np.diag(Sij)**(-1/2))
+        for Sij in CPSD]
+    return np.asarray(cohere)
