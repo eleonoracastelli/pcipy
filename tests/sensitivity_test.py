a063d22f1794d648afcd06d42ea5d76fc0a8ae9d
Eleonora Castelli <ell.castelli@gmail.com>
2025-05-14 16:58:20 -0400

    Start testing sensitivity

---
+++ tests/sensitivity_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 14 16:56:48 2025
+
+@author: ecastel2
+"""
+
+import numpy as np
+import os
+from backgrounds import utils, loadings, plotting, noise, tdi, analysis
