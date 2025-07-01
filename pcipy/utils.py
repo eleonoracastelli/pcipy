5a2cad1a0904bd7bec5c8d41666d84d0e1cd335c
Eleonora Castelli <ell.castelli@gmail.com>
2025-07-01 15:32:20 -0400

    Add licensing to all Eleonora's scripts

--- pcipy/utils.py
+++ pcipy/utils.py
@@ -16,7 +16,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 File: utils.py
-
 Purpose: This module provides utility functions for estimating the Power Spectral Density (PSD) of signals.
 
 The functions included in this module facilitate various methods of PSD estimation, including
