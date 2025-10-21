60d31a41f56b21ecc8156987f536e548cf8d9e51
Eleonora Castelli <ell.castelli@gmail.com>
2025-10-21 16:46:18 -0400

    Add licensing

--- pcipy/sensitivity.py
+++ pcipy/sensitivity.py
@@ -4,6 +4,21 @@
 Created on Tue Dec 10 15:41:14 2024
 
 @author: E Castelli, J Baker
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+File: sensitivity.py
+Purpose: This module provides functions for estimating the sensitivity of data formatted as TimeData objects.
 """
 
 import numpy as np
