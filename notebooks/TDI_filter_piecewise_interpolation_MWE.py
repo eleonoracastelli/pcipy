9320dce6e8748d7dd4b940959507acbf62365a26
Eleonora Castelli <ell.castelli@gmail.com>
2025-11-11 17:18:44 -0500

    Adapt number of kernel evaluations to length of simulation

--- notebooks/TDI_filter_piecewise_interpolation_MWE.py
+++ notebooks/TDI_filter_piecewise_interpolation_MWE.py
@@ -52,7 +52,7 @@ from pyfftw.interfaces.numpy_fft import rfft
 # %% In[2]: 1. Read in data sets
 SKIP = 1000
 
-RANGE_IN_HOURS = 72
+RANGE_IN_HOURS = 24*14
 
 DATADIR = "/Users/ecastel2/Documents/research/GSFC/pci-inrep/"
 WORKDIR = DATADIR+"/simulations/"
@@ -277,7 +277,10 @@ def timed_filter(data_noise, t, in_chans):
     print(f"Time taken for: {end_time - start_time:.4f} seconds")
     return result
 
-nks = [1, 2, 4, 8]#, 16, 32] # 3.75 s per iteration
+
+optimal_nks = RANGE_IN_HOURS // 6
+
+nks = [1, optimal_nks//8, optimal_nks//4, optimal_nks//2, optimal_nks] # 3.75 s per iteration
 tks = [PiecewiseFilter.select_kernel_times(ytest_n, nk) for nk in nks]
 filtsets = [[TDIFilter(data_noise, eval_time=t, in_chans=in_chans, method='linear')
              for t in PiecewiseFilter.select_kernel_times(ytest_n, nk)]
